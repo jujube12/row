57c575ceef9c863be898b89f20239c62eff50fbc
jujube12 <gyubam1@gmail.com>
2024-03-04 22:34:22 +0900

    feat: 회원가입 UI 구현 - 로그인페이지와 통일

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,20 @@
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth'
 import AlreadyLogged from '../login/alreayLogged'
+import style from './register.module.css'
 export default async function Register() {
     let session = await getServerSession(authOptions)
     return (
         session
             ? <AlreadyLogged></AlreadyLogged>
-            : <div>
+            : <div className={`${style.register_container} container`}>
+                <div className={style.register_title}>
+                    Row
+                </div>
                 <form action='/api/user/signup' method="POST">
-                    <input name='name'></input>
-                    <input name='email'></input>
-                    <input name='password'></input>
+                    <input name='name' placeholder='name'></input>
+                    <input name='email' placeholder='email'></input>
+                    <input name='password' placeholder='password'></input>
                     <button type='submit'>sign up</button>
                 </form>
             </div>
