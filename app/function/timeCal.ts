d07c9f137d161b6337d6f36b73312523bb73c1d1
jujube12 <gyubam1@gmail.com>
2024-03-21 23:48:43 +0900

    fix: 전적 상세 페이지 매치 지난 시간 구하는 함수 수정 - 현재 시간을 기준으로 게임 시작시간과의 차이를 나타내주는 함수의 조건문 수정

--- app/function/timeCal.ts
+++ app/function/timeCal.ts
@@ -15,26 +15,28 @@ export function matchDate(unixTime: number): date {
     }
     return obj
 }
+// 게임 진행 시간
 export function duringTime(unixTime: number): { min: number, sec: number } {
     let time = unixTime / 60
     let minutes = Math.trunc(time)
     let second = Number((time % 1 * 60).toFixed(3))
     return { min: minutes, sec: second }
 }
-
+// 게임 일시 지난 정도
 export function gameDate(unixTime: number): string {
     const now = new Date();
     const game = new Date(unixTime);
     const diff = now.getTime() - game.getTime()
+    console.log(diff)
     let result: string = ''
-    if (diff > (30 * 24 * 60 * 60 * 1000)) {
-        result = '오래전'
-    } else if (diff >= (24 * 60 * 60 * 1000)) {
+    if (diff >= (30 * 24 * 60 * 60 * 1000)) {
+        result = '오래전'  // 30일 이상
+    } else if (diff >= (24 * 60 * 60 * 1000) && diff < (30 * 24 * 60 * 60 * 1000)) {  //1일 이상 30일 미만
         result = Math.floor(diff / (24 * 60 * 60 * 1000)) + '일 전'
-    } else if (diff < (24 * 60 * 60 * 1000) && diff > (60 * 60 * 1000)) {
+    } else if (diff < (24 * 60 * 60 * 1000) && diff >= (60 * 60 * 1000)) { //1시간 이상 24시간 미만
         result = Math.floor(diff / (60 * 60 * 1000)) + '시간 전'
     } else if (diff < (60 * 60 * 1000)) {
-        result = Math.floor(diff / (60 * 60 * 1000)) + '분 전'
+        result = Math.floor(diff / (60 * 60 * 1000)) + '분 전' //1시간 미만
     }
     return result
 }
