ec0916d1f3df12bd81eb5d325e1046bf3e9a5b55
jujube12 <gyubam1@gmail.com>
2024-03-28 00:28:24 +0900

    feat: champ page 포지션 별 챔피언 토글 박스 생성 - db에 저장된 정보를 매번 불러오는 것보다 배열에 정보를 저장해두는 것이 좋을 것 같아서 배열을 만들어두고 챔피언 포지션별 이미지를 불러옴

---
+++ app/champ/champToggleBox.tsx
@@ -0,0 +1,107 @@
+'use client'
+import style from './champ.module.css'
+import { useState } from 'react'
+export default function ChampToggleBox() {
+    let champPosition = [[
+        'Akali', 'Ekko', 'Evelynn',
+        'Kassadin', 'Katarina', 'Khazix',
+        'Fizz', 'MasterYi', 'Naafiri',
+        'Nidalee', 'Nocturne', 'Leblanc',
+        'Rengar', 'Shaco', 'Talon',
+        'Viego', 'Qiyana', 'Zed',
+        'Yone'
+    ], [
+        'Aatrox', 'Belveth', 'Briar',
+        'Camille', 'Diana', 'Darius',
+        'DrMundo', 'Fiora', 'Gangplank',
+        'Garen', 'Gnar', 'Gwen',
+        'Hecarim', 'Irelia', 'Illaoi',
+        'Jayce', 'Jax', 'Kayle',
+        'Kayn', 'Kled', 'LeeSin',
+        'Lillia', 'MonkeyKing', 'Mordekaiser',
+        'Nilah', 'Nasus', 'Olaf',
+        'Pantheon', 'Gragas', 'RekSai',
+        'Renekton', 'Rumble', 'Riven',
+        'Shyvana', 'Sett', 'Skarner',
+        'Trundle', 'Tryndamere', 'Udyr',
+        'Urgot', 'Vi', 'Volibear',
+        'Warwick', 'Yasuo', 'Yorick',
+        'XinZhao'
+    ], [
+        'Ahri', 'Anivia', 'Annie',
+        'AurelionSol', 'Azir', 'Cassiopeia',
+        'Elise', 'Fiddlesticks', 'Brand',
+        'Heimerdinger', 'Hwei', 'Karthus',
+        'Karma', 'Kennen', 'Lissandra',
+        'Malzahar', 'Lux', 'Morgana',
+        'Neeko', 'Orianna', 'Ryze',
+        'Seraphine', 'Swain', 'Sylas',
+        'Syndra', 'Taliyah', 'TwistedFate',
+        'Velkoz', 'Veigar', 'Vex',
+        'Viktor', 'Vladimir', 'Zyra',
+        'Ziggs', 'Zoe', 'Xerath'
+    ], [
+        'Aphelios', 'Ashe', 'Akshan',
+        'Caitlyn', 'Corki', 'Draven',
+        'Ezreal', 'Graves', 'Jhin',
+        'Kalista', 'Kaisa', 'Jinx',
+        'Kindred', 'KogMaw', 'Lucian',
+        'MissFortune', 'Quinn', 'Samira',
+        'Senna', 'Sivir', 'Smolder',
+        'Teemo', 'Tristana', 'Twitch',
+        'Varus', 'Vayne', 'Xayah',
+        'Zeri'
+    ], [
+        'Bard', 'Braum',
+        'Janna', 'Ivern',
+        'Lulu', 'Milio',
+        'Nami', 'Pyke',
+        'Rakan', 'Renata',
+        'TahmKench', 'Sona',
+        'Soraka', 'Taric',
+        'Thresh', 'Yuumi',
+        'Zilean'
+    ], [
+        'Amumu', 'Alistar',
+        'Chogath', 'Blitzcrank',
+        'Galio', 'JarvanIV',
+        'KSante', 'Leona',
+        'Malphite', 'Maokai',
+        'Nautilus', 'Nunu',
+        'Ornn', 'Poppy',
+        'Rammus', 'Singed',
+        'Shen', 'Sejuani',
+        'Sion', 'Rell',
+        'Zac'
+    ]]
+    let [position, setPosition] = useState(0)
+
+    function champImg(position: any) {
+        let arr = champPosition[position]
+        return (
+            champPosition[position].map((a, i) => {
+                return (
+                    <div key={i} className={style.champInfo_box}>
+                        <div>
+                            <img src={`https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${a}.png`}></img>
+                        </div>
+                        <div>{a}</div>
+                    </div>
+                )
+            })
+        )
+    }
+    return (
+        <div className={style.champList_wrapper}>
+            <div className={style.champList_toggle_btn}>
+                <div className={position == 0 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(0) }}>암살자</div>
+                <div className={position == 1 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(1) }}>전사</div>
+                <div className={position == 2 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(2) }}>마법사</div>
+                <div className={position == 3 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(3) }}>원거리딜러</div>
+                <div className={position == 4 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(4) }}>서포터</div>
+                <div className={position == 5 ? style.toggle_btn_selected : ''} onClick={() => { setPosition(5) }}>탱커</div>
+            </div>
+            <div className={style.champList_box}>{champImg(position)}</div>
+        </div>
+    )
+}
--- app/champ/page.tsx
+++ app/champ/page.tsx
@@ -1,29 +1,11 @@
 import style from './champ.module.css'
 import { connectDB } from '@/util/database'
-import ChampToggleBtn from './champToggleBtn'
+import ChampToggleBox from './champToggleBox'
 export default async function champ() {
-    const db = (await connectDB).db(process.env.NEXT_DB_NAME)
-    let result = await db.collection('Support').find().toArray()
     return (
         <div className={style.champ_container}>
             <div className={style.champ_title}>Row</div>
-            <div className={style.champList_wrapper}>
-                <ChampToggleBtn></ChampToggleBtn>
-                <div className={style.champList_box}>
-                    {
-                        result.map((a, i) => {
-                            return (
-                                <div key={i} className={style.champInfo_box}>
-                                    <div>
-                                        <img src={`https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${a.champName}.png`}></img>
-                                    </div>
-                                    <div>{a.champName}</div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+            <ChampToggleBox></ChampToggleBox>
         </div>
     )
 }
