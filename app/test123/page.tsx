49d96725483061fa73c45093812b5919feedf1bc
jujube12 <gyubam1@gmail.com>
2024-04-10 23:34:39 +0900

    feat: [champ build] 통계 데이터 수집 - 매치의 queueId 가 420 이면서 버전이 가장 최신 버전(오늘 기준 14.7)인 매치 500개의 정보를 수집 - lol api의 요청 제한이 1초에 20개 2분에 100 개이므로 시간 간격을 두어 요청을 해줘야 함

--- app/test123/page.tsx
+++ app/test123/page.tsx
@@ -1,37 +1,43 @@
 import { connectDB } from "@/util/database"
 
 export default async function Test() {
-    // let db = (await connectDB).db(process.env.NEXT_DB_NAME)
-    // let a = await db.collection('champ').find().toArray()
+    let db = (await connectDB).db(process.env.NEXT_DB_NAME)
 
-    // a.map((t, i) => {
-    //     console.log(t.champName)
-    //     fetch(`https://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/champion/${t.champName}.json`)
+    let num = 7015674700
+    let cnt = 0
+    let conti = true
+    let bre = 0
+    // await fetch(`https://asia.api.riotgames.com/lol/match/v5/matches/KR_7015674994?api_key=${process.env.NEXT_RIOT}`)
+    //     .then((r) => r.json())
+    //     .then((result) => {
+    //         console.log(result)
+    //     })
+
+    // while (conti) {
+    //     await fetch(`https://asia.api.riotgames.com/lol/match/v5/matches/KR_${num}?api_key=${process.env.NEXT_RIOT}`)
     //         .then((r) => r.json())
     //         .then(async (result) => {
-    //             result.name = t.champName
-    //             await db.collection('champInfo').insertOne(result)
-    //         })
-    // })
-
-    // let Fighter: any = new Set([])
-    // let Assassin: any = new Set([])
-    // let Mage: any = new Set([])
-    // let Tank: any = new Set([])
-    // let Marksman: any = new Set([])
-    // let Support: any = new Set([])
-    // let aa = [Fighter, Assassin, Mage, Tank, Marksman, Support]
-    // let l = ['Fighter', 'Assassin', 'Mage', 'Tank', 'Marksman', 'Support']
-    // l.map((j, m) => {
-    //     a.map((i) => {
-    //         if (i.champInfo.tags[0] == j) {
-    //             aa[m].add(i.champName)
-    //             if (i.champInfo.tags[1] == j) {
-    //                 aa[m].add(i.champName)
+    //             if (result.info) {
+    //                 if (result.info.gameVersion.slice(0, 4) == '14.7' && result.info.queueId == 420) {
+    //                     // await db.collection('match_14.7').insertOne(result)
+    //                     cnt += 1
+    //                     console.log('저장 완료: ' + result.info.gameId)
+    //                     bre = 0
+    //                 }
+    //                 console.log('있지만 저장 안함: ' + result.info.gameId)
+    //             } else {
+    //                 bre += 1
+    //                 console.log('없음: ' + num)
     //             }
-    //         }
-    //     })
-    // })
+    //         })
+    //     num += 1
+    //     if (cnt > 499 || bre > 100) {
+    //         conti = false
+    //     }
+    // }
+
+    console.log(cnt)
+    console.log(num)
 
     return (
         <div>
